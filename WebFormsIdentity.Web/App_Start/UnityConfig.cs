da5331e44e6a72c5aebffe535ffaf4ac4b6caf5f
Dimitar Kostov <dimitar.i.kostov@gmail.com>
2021-03-27 00:58:38 +0200

    Add Unity support

---
+++ WebFormsIdentity.Web/App_Start/UnityConfig.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.WebFormsDependencyInjection.Unity;
+using System;
+using System.Web;
+using Unity;
+using Unity.Injection;
+using Unity.Lifetime;
+using WebFormsIdentity.Data.EntityFramework;
+using WebFormsIdentity.Domain;
+using WebFormsIdentity.Identity;
+
+namespace WebFormsIdentity
+{
+    public static class UnityConfig
+    {
+        public static void RegisterComponents(HttpApplication application)
+        {
+            var container = application.AddUnity();
+
+            container.RegisterType<IUnitOfWork, UnitOfWork>(new HierarchicalLifetimeManager(), new InjectionConstructor("DefaultConnection"));
+            container.RegisterType<IUserStore<IdentityUser, Guid>, UserStore>(new TransientLifetimeManager());
+            container.RegisterType<RoleStore>(new TransientLifetimeManager());
+        }
+    }
+}
